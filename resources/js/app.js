4936db861591cb33a860547f5bb9435b231294d3
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-05-20 00:14:45 +0200

    Add ChatComponent to Vue instance in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import { createApp } from "vue";
+import ChatComponent from "./components/ChatComponent.vue";
+
+createApp({
+    components: {
+        ChatComponent,
+    },
+}).mount("#app");
