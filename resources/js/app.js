b3dc81cc286c9d19ffeb450c614511518108a58a
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-05-20 00:20:13 +0200

    Add Laravel Echo and configure it to use socket.io for real-time chat functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,13 @@ require("./bootstrap");
 
 import { createApp } from "vue";
 import ChatComponent from "./components/ChatComponent.vue";
+import Echo from "laravel-echo";
+
+window.io = require("socket.io-client");
+window.Echo = new Echo({
+    broadcaster: "socket.io",
+    host: window.location.hostname + ":6001", // the host of your Laravel Echo server
+});
 
 createApp({
     components: {
