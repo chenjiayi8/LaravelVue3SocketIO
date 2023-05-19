ac91fab8c0015f9c65295aee240764fd034feaf4
Jiayi Chen <chenjiayi_344@hotmail.com>
2023-05-20 01:09:12 +0200

    Move message routes to API controller

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,3 @@ use App\Http\Controllers\MessageController;
 Route::get('/', function () {
     return view('index');
 });
-
-
-
-Route::get('/messages', [MessageController::class, 'index']);
-Route::post('/messages', [MessageController::class, 'store']);
